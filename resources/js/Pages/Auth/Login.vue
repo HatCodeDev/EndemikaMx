98ac5aa532320e485aa8b21dee3400c556457479
HatCodeDev <misael.reynoso.aguayo@gmail.com>
2024-11-10 14:02:56 -0600

    feat(diseño) -login -register

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -35,7 +35,7 @@ const submit = () => {
 		
 		<div class="flex flex-col overflow-y-auto md:flex-row">
 			<div class="h-32 md:h-auto md:w-1/2">
-				<img aria-hidden="true" class="object-cover w-full h-full" src="/images/login-office.jpeg" alt="Office" />
+				<img aria-hidden="true" class="object-cover w-full h-full" src="/images/login.jpg" alt="Office" />
 			</div>
 			<div class="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
 				<div class="w-full">
@@ -61,14 +61,22 @@ const submit = () => {
 						<div class="block mt-4">
 							<label class="flex items-center">
 								<Checkbox name="remember" v-model:checked="form.remember" />
-								<span class="ml-2 text-sm text-gray-600">Remember me</span> </label>
+								<span class="ml-2 text-sm text-gray-600">Remember me</span> 
+							</label>
 						</div>
 						
-						<div class="flex items-center justify-end mt-4">
-							<Link v-if="canResetPassword" :href="route('password.request')" class="text-sm text-gray-600 underline  hover:text-gray-900">
+						<div class="flex flex-col sm:flex-row items-center justify-end mt-4 gap-4">
+							<!-- Enlace para recuperar contraseña -->
+							<Link v-if="canResetPassword" :href="route('password.request')" class="text-sm text-gray-600 underline hover:text-gray-900">
 								Forgot your password?
 							</Link>
 							
+							<!-- Botón para registrarse -->
+							<Link href="/register" class="text-sm text-gray-600 underline hover:text-gray-900">
+								Register
+							</Link>
+							
+							<!-- Botón para iniciar sesión -->
 							<PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
 								Log in
 							</PrimaryButton>
@@ -79,3 +87,4 @@ const submit = () => {
 		</div>
 	</GuestLayout>
 </template>
+
